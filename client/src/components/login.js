642d50641091b6a58401f9867946379bf2ab543b
lunify <51797974+lunify@users.noreply.github.com>
2019-08-27 23:40:32 +0100

    add login component logic

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react'
+import { auth } from '../services/api'
 
 export default Login
 
-function Login() {
+function Login({ onLogin }) {
+  // state ********************
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState([])
+  const [loading, setLoading] = useState(false)
 
+  // methods ********************
+  async function handleSubmit(e) {
+    e.preventDefault()
+    setLoading(true)
+
+    const { errors, user } = await auth.login({ username, password })
+
+    if (errors) {
+      setLoading(false)
+      return setErrors(errors)
+    }
+
+    onLogin(user)
+  }
+
+  // render ********************
   return (
     <div>
       <header>
         <h2>Login form</h2>
+        {errors && errors.map(error => (
+          <p key={error}>{error}</p>
+        ))}
       </header>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
         <input
           onChange={e => setUsername(e.target.value)}
@@ -34,9 +57,8 @@ function Login() {
           required
         />
 
-        <button>Sign in</button>
+        <button disabled={loading}>Sign in</button>
       </form>
     </div>
   );
 }
-
