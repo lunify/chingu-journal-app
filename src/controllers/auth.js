1ebc434410981e471e5c041c8ff7874e1a524abc
lunify <51797974+lunify@users.noreply.github.com>
2019-08-27 21:24:39 +0100

    implement register controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,32 @@
+const UserModel = require('../models/user')
+
 module.exports = { register, login }
 
 async function register(req, res) {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    return res.status(400).send()
+  }
+
+  try {
+    var user = await UserModel.create({ username, password })
+  } catch (err) {
+    if (err.name == 'ValidationError') {
+      const response = []
+      for (error in err.errors) {
+        response.push(err.errors[error].message)
+      }
+
+      return res.json({ errors: response })
+    } else if (err.name == 'MongoError' && err.code == 11000) {
+      return res.json({ errors: ['username already exists'] })
+    }
+
+    return res.status(500).send()
+  }
+
+  return res.json({ user: user.toObject() })
 }
 
 async function login(req, res) {
