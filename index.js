a6ba7d3f252980cb52727c45af55d714395c5d95
lunify <51797974+lunify@users.noreply.github.com>
2019-08-27 19:34:54 +0100

    add dotenv for dev environment

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config({ path: '.env.development.local' })
+}
+
 const express = require('express')
 const morgan = require('morgan')
 
