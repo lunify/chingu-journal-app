5a2cc873c6926c74312095bd5133a3e007ef16e4
lunify <51797974+lunify@users.noreply.github.com>
2019-08-27 18:45:03 +0100

    add deployment code for heroku

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ app.get('/api', (req, res) => {
   res.json({ message: `${req.method} request on ${req.path} has been made` })
 })
 
+if (process.env.NODE_ENV == 'production') {
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => { console.info(`Successfuly connected to port ${PORT}`) })
