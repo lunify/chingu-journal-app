c16047124a24010205a2215f5442a058b53d4391
lunify <51797974+lunify@users.noreply.github.com>
2019-08-28 07:17:43 +0100

    add note creation controller

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -1,4 +1,37 @@
+const UserModel = require('../models/user')
+
 module.exports = { createNote }
 
 async function createNote(req, res) {
+  const { title, body } = req.body
+  const { userId } = req.query
+
+  if (!title || !body || !userId) {
+    return res.status(400).send()
+  }
+
+  try {
+    var user = await UserModel.findOneAndUpdate(
+      { _id: userId },
+      { $addToSet: { notes: { title, body } } },
+      { new: true, runValidators: true }
+    )
+  } catch (err) {
+    if (err.name == "ValidationError") {
+      const response = [];
+      for (error in err.errors) {
+        response.push(err.errors[error].message);
+      }
+
+      return res.json({ errors: response });
+    }
+
+    return res.status(500).send()
+  }
+
+  if (!user) {
+    return res.json({ errors: ['user not found'] })
+  }
+
+  return res.json({ notes: user.toObject().notes })
 }
