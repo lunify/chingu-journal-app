c42935eb7bfd7073e74e91e553d32a6ddc1172cf
lunify <51797974+lunify@users.noreply.github.com>
2019-08-28 02:27:25 +0100

    save user in app state on successful login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from './components/login'
 import Register from './components/register'
 
 function App() {
+  const [user, setUser] = useState([])
   return (
     <Router>
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
+      <Route path="/register" render={() => <Register onRegister={setUser} />} />
+      <Route path="/login" render={() => <Login onLogin={setUser} />} />
     </Router>
   )
 }
