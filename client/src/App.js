a1d527af4c9d8627f027bac07db46555fa09d0fb
lunify <51797974+lunify@users.noreply.github.com>
2019-08-28 02:37:11 +0100

    redirect on successful authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
 import Login from './components/login'
 import Register from './components/register'
 
 function App() {
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState(null)
   return (
     <Router>
-      <Route path="/register" render={() => <Register onRegister={setUser} />} />
-      <Route path="/login" render={() => <Login onLogin={setUser} />} />
+      <Route
+        path="/register"
+        render={() =>
+          user ? <Redirect to="/" /> : <Register onRegister={setUser} />
+        }
+      />
+      <Route
+        path="/login"
+        render={() =>
+          user ? <Redirect to="/" /> : <Login onLogin={setUser} />
+        }
+      />
     </Router>
   )
 }
